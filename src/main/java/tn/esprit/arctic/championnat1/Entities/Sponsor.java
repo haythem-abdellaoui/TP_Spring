a16c29bdaf2c67b6807b8da6305948e869c2c383
SiHaythem420 <SiHaythem420>
2026-02-13 11:11:53 +0100

    CRUD

--- src/main/java/tn/esprit/arctic/championnat1/Entities/Sponsor.java
+++ src/main/java/tn/esprit/arctic/championnat1/Entities/Sponsor.java
@@ -1,10 +1,20 @@
 package tn.esprit.arctic.championnat1.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Sponsor")
 public class Sponsor implements Serializable {
 
@@ -20,6 +30,10 @@ public class Sponsor implements Serializable {
     private Float floatBudgetAnnuel;
 
     private Boolean booleanBloquerContrat;
+    Boolean archived;
+    LocalDate dateCreation;
+    LocalDate dateDerniereModification;
+
 
     @OneToMany(mappedBy = "sponsor")
     private List<Contrat> contratList;
