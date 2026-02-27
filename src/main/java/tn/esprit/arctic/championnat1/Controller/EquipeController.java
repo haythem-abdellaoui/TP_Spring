d3c60dc466297fe963a4242c2926428236449f2c
SiHaythem420 <SiHaythem420>
2026-02-27 15:33:11 +0100

    Ajouter Equipe

--- src/main/java/tn/esprit/arctic/championnat1/Controller/EquipeController.java
+++ src/main/java/tn/esprit/arctic/championnat1/Controller/EquipeController.java
@@ -2,12 +2,18 @@ package tn.esprit.arctic.championnat1.Controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.arctic.championnat1.Entities.Equipe;
 import tn.esprit.arctic.championnat1.Services.IEquipeService;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/equipes")
 public class EquipeController {
     
     private IEquipeService equipeService;
+    @PostMapping("/ajouter_equipe")
+    public void ajouterEquipe(@RequestBody Equipe equipe) {
+        equipeService.ajouterEquipe(equipe);
+    }
 
 }
